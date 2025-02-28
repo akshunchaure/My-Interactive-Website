dfaad1927e21fe5f5fea98c0989c2944e8deb2a7
akshunchaure <gr8akshunchaure@gmail.com>
2025-02-28 15:17:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const myParagraph = document.getElementById("my-paragraph"); // Gets the paragraph element
+const myButton = document.getElementById("my-button");     // Gets the button element
+
+myButton.addEventListener("click", function() {  // Listens for a click on the button
+    myParagraph.textContent = "Shweta you are one of the most beautiful girls I ever met in my entire life. You may think that I am overdoing it but whatever I am saying is coming straight from my heart. You are cute and I haven't even met you. This means your heart is pure because I can feel those vibes from far. Stay blessed. May Mahadev bless you with every beautiful thing in life";  // Changes the paragraph text
+});
